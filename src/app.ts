3ab8be01ad4ba3c5e2d265347174382ed492b18a
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-05-23 07:51:43 +0600

    initialize project with TypeScript, Express, and PostgreSQL setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { type Application, type Request, type Response } from "express"
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
+
+app.get("/", (req: Request, res: Response) => {
+  
+  res.status(200).json({
+    message: "Devpulse",
+    author: "Platform for developer",
+  });
+});
+
+
+
+
+export default app;
